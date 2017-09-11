1b523644dd4012734e6740faf55a8b8dc547686d
Subhranil Mukherjee <subhranilmukherjee190@gmail.com>
2017-09-11 23:14:58 +0530

    poly_driver : be more interactive

--- poly_driver.c
+++ poly_driver.c
@@ -3,10 +3,15 @@
 #include <stdio.h>
 
 int main(){
-    Polynomial p1 = poly_new(3, 1);
-    poly_concat(p1, poly_new(15, 0));
-
-    Polynomial p2 = poly_new(-3, 0);
+    printf("\n[Enter all polynomials as f(x)!]");
+    printf("\nEnter first polynomial : ");
+    char *buffer;
+    readline(&buffer, stdin);
+    Polynomial p1 = poly_parse(buffer, 'x');
+    printf("\nEnter second polynomial : ");
+    buffer = NULL;
+    readline(&buffer, stdin);
+    Polynomial p2 = poly_parse(buffer, 'x');
 //    poly_concat(p2, poly_new(15, 0));
 
     printf("\nPolynomial 1 : ");
@@ -24,24 +29,23 @@ int main(){
     Polynomial res = poly_pwrof(p1, 3);
     printf("\np1^3 : ");
     poly_print(res);
-    printf("\n");
-    Polynomial p = poly_new(3, 2);
-    poly_concat(p, poly_new(2, 1));
+    printf("\nEnter another polynomial to replace each term of p1 with : ");
+    buffer = NULL;
+    readline(&buffer, stdin);
+    Polynomial p = poly_parse(buffer, 'x');
     res = poly_replace(p1, p);
     printf("\nf(x) = ");
     poly_print(p1);
     printf("\nnewX = ");
     poly_print(p);
-    printf("\nf(3x + 2) = ");
+    printf("\nf(");
+    poly_print(p);
+    printf(") = ");
     poly_print(res);
-
-    printf("\nEnter a polynomial : ");
-    char *line = NULL;
-    readline(&line, stdin);
-    Polynomial convert = poly_parse(line, 'x');
-    printf("\nParsed polynomial is : ");
-    poly_print(convert);
-
+    printf("\nEnter the value of x to evaluate p1 : ");
+    double d;
+    scanf("%lf", &d);
+    printf("\nResult : %g", poly_eval(p1, d));
     printf("\n");
     return 0;
 }
